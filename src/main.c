51180590c79127948faa9c7934404dd3f6b6c0d4
James Cox-Morton <james.cox-morton@arm.com>
2025-06-25 09:34:23 +0100

    Remove unused import

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 
 #include <stdio.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h> /* or <zephyr/printk.h> */
 
 // https://github.com/zephyrproject-rtos/zephyr/blob/main/boards/nxp/imx93_evk/imx93_evk_mimx9352_m33.dts
 
 int main(void) {
-  printk("Hello World from the %s\n", CONFIG_BOARD);
+  printk("Hello Computer from the %s\n", CONFIG_BOARD);
   return 0;
 }
