97d9094eeef9d82e6bc539d563b7a0d246b46690
Rob Moran <rob.moran@arm.com>
2025-08-04 17:59:51 +0100

    Update main.c

--- workdir/src/main.c
+++ workdir/src/main.c
@@ -11,6 +11,9 @@
 // https://github.com/zephyrproject-rtos/zephyr/blob/main/boards/nxp/imx93_evk/imx93_evk_mimx9352_m33.dts
 
 int main(void) {
-  printk("Hello Computer from the %s\n", CONFIG_BOARD);
+  for (int i = 0; i < 50; i++) {
+    printf("Hello World %d from the %s\n", i, CONFIG_BOARD);
+    k_msleep(500); // Sleep for 500 milliseconds
+  }
   return 0;
 }
